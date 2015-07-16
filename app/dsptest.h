5a030e01147851b883fcd6fb80f936a86c884592
rentt <rentt@playuav.com>
2015-07-16 14:44:19 +0800

    code refact

--- app/dsptest.h
+++
@@ -1,6 +0,0 @@
-#include "arm_math.h" 
-#include "math_helper.h" 
-
-void matrix_init(arm_matrix_instance_f32* mat, arm_matrix_instance_f32* mat2);
-void matrix_cmp(void);
-int32_t matrix_test(void);
